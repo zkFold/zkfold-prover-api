9dbca07e50dc70f6d8e6f3e9ebd3a5bc9fb2a3eb
Vardominator <vbarsegyan@gomaestro.org>
2024-06-12 04:15:11 +0000

    some cleanup

--- src/Types/ZkProof.hs
+++ src/Types/ZkProof.hs
@@ -1,26 +1,27 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Types.ZkProof where
 
-import GHC.Generics (Generic)
 import Control.DeepSeq (NFData)
-import ZkFold.Base.Protocol.NonInteractiveProof
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS (empty)
+import GHC.Generics (Generic)
+import ZkFold.Base.Protocol.NonInteractiveProof
 
--- Define your proof type
 data ZkProof = ZkProof
   deriving (Show, Eq, Generic, NFData)
 
 instance NonInteractiveProof ZkProof where
   type Transcript ZkProof = ByteString
-  type Setup ZkProof = ByteString
+  type SetupProve ZkProof = ByteString
+  type SetupVerify ZkProof = ByteString
   type Witness ZkProof = ByteString
   type Input ZkProof = ByteString
   type Proof ZkProof = ByteString
 
-  setup _ = BS.empty
+  setupProve _ = BS.empty
+  setupVerify _ = BS.empty
   prove _ _ = (BS.empty, BS.empty)
   verify _ _ _ = True
