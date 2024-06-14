b4c59c3edb599b0e53c861e002d266d14df1aea5
Murat Kasimov <iokasimov.m@gmail.com>
2024-06-14 18:17:27 +0700

    Generate Swagger documentation

--- src/Types/Instances.hs
+++ src/Types/Instances.hs
@@ -1,47 +1,48 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Types.Instances where
 
+import Control.Lens hiding ((.=))
 import GHC.Generics (Generic)
-import Data.Aeson (ToJSON, FromJSON, toJSON, parseJSON, withObject, (.:), (.=), object)
+import Data.Swagger
+import Data.Aeson
+import Data.Text
+import Data.Aeson.Types
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as BS
-import qualified Data.Text as T
-import qualified Data.Aeson.Key as Key
-import ZkFold.Base.Protocol.NonInteractiveProof (ProveAPIResult(..))
+import ZkFold.Base.Protocol.NonInteractiveProof (ProveAPIResult(..), ProofBytes (..))
 
--- Define ToJSON and FromJSON instances for ByteString
-instance ToJSON BS.ByteString where
-  toJSON = Aeson.String . T.pack . BS.unpack . B64.encode
-
-instance FromJSON BS.ByteString where
-  parseJSON = Aeson.withText "ByteString" $ \t ->
-    case B64.decode (BS.pack (T.unpack t)) of
-      Left err -> fail err
-      Right bs -> return bs
-
--- Define ToJSON and FromJSON instances for ProveAPIResult
 instance ToJSON ProveAPIResult where
-  toJSON (ProveAPISuccess bs) = 
-    object [Key.fromString "status" .= ("success" :: String), Key.fromString "data" .= bs]
-  toJSON ProveAPIErrorSetup = 
-    object [Key.fromString "status" .= ("error" :: String), Key.fromString "message" .= ("Setup error" :: String)]
-  toJSON ProveAPIErrorWitness = 
-    object [Key.fromString "status" .= ("error" :: String), Key.fromString "message" .= ("Witness error" :: String)]
+  toJSON (ProveAPISuccess bs) = object
+    [ "status" .= ("success" :: String)
+    , "data" .= bs
+    ]
+  toJSON ProveAPIErrorSetup = object
+    [ "status" .= ("error" :: String)
+    , "message" .= ("Setup error" :: String)
+    ]
+  toJSON ProveAPIErrorWitness = object
+    ["status" .= ("error" :: String)
+    , "message" .= ("Witness error" :: String)
+    ]
 
 instance FromJSON ProveAPIResult where
-  parseJSON = withObject "ProveAPIResult" $ \v -> do
-    status <- v .: Key.fromString "status"
-    case (status :: String) of
-      "success" -> do
-        dataStr <- v .: Key.fromString "data"
-        return $ ProveAPISuccess dataStr
-      "error" -> do
-        message <- v .: Key.fromString "message"
-        case (message :: String) of
-          "Setup error" -> return ProveAPIErrorSetup
-          "Witness error" -> return ProveAPIErrorWitness
-          _ -> fail "Unknown error message"
+  parseJSON = withObject "ProveAPIResult" $ \v ->
+    v .: "status" & id @(Parser String) >>= \case
+      "success" -> ProveAPISuccess <$> v .: "data"
+      "error" -> v .: "message" & id @(Parser String) >>= \case
+        "Setup error" -> return ProveAPIErrorSetup
+        "Witness error" -> return ProveAPIErrorWitness
+        _ -> fail "Unknown error message"
       _ -> fail "Unknown status"
+
+instance ToSchema ProofBytes where
+  declareNamedSchema _ = pure $ NamedSchema (Just "Proof bytes") byteSchema
+
+instance ToSchema ProveAPIResult where
+  declareNamedSchema = genericDeclareNamedSchemaUnrestricted defaultSchemaOptions
