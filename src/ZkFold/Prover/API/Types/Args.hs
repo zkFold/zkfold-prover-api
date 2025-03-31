d151c39ba2011f6fccc0ed175749bf5c3abe0d72
Eugene Samodelov <geli1610@yandex.ru>
2025-03-31 18:15:31 +0300

    feat: add simple contract proving

--- src/ZkFold/Prover/API/Types/Args.hs
+++ src/ZkFold/Prover/API/Types/Args.hs
@@ -1,21 +1,23 @@
 module ZkFold.Prover.API.Types.Args where
 
-import           Data.ByteString (ByteString)
+import           Control.DeepSeq             (NFData)
+import           Data.Aeson                  (FromJSON, ToJSON)
+import           Data.ByteString             (ByteString)
 import           Data.Swagger
-import           GHC.Generics    (Generic)
+import           GHC.Generics                (Generic)
 import           Prelude
-import           Servant         (MimeUnrender, OctetStream)
-
+import           Servant                     (MimeUnrender, OctetStream)
+import           ZkFold.Base.Data.ByteString
 newtype SetupBytes
   = SetupBytes ByteString
-    deriving (Generic, MimeUnrender OctetStream)
+    deriving (Generic, FromJSON)
 
 instance ToSchema SetupBytes where
   declareNamedSchema _ = pure $ NamedSchema (Just "Setup bytes") byteSchema
 
 newtype WitnessBytes
   = WitnessBytes ByteString
-    deriving (Generic, MimeUnrender OctetStream)
+    deriving (Generic, FromJSON)
 
 instance ToSchema WitnessBytes where
   declareNamedSchema _ = pure $ NamedSchema (Just "Witness bytes") byteSchema
